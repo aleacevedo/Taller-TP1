3608462869fce817be76ee4b9851e7504e8d93cc
aleacevedo <ale.acevedo@live.com.ar>
2019-08-29 12:59:11 -0300

    Modify server

--- server.c
+++ server.c
@@ -1,9 +1,14 @@
 #include "server.h"
 #include <stdio.h>
 #include "sudoku.h"
+#include "board.h"
 
-void server_init(const char* service) {
+void server_init(const char *service)
+{
+  char boardRepresentation[BOARD_REPRESENTATION_LEN] = "\0";
   sudoku_t sudoku;
-  sudoku_init(sudoku);
+  sudoku_init(&sudoku);
+  sudoku_show(&sudoku, boardRepresentation);
   printf("Levanto un server en el puerto: %s \n", service);
+  printf("%s \n", boardRepresentation);
 }
