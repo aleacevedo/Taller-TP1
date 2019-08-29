8c913a4f11d4c61cb3d3684a45cc159f5515bf8c
alejoacevedo <ale.acevedo@live.com.ar>
2019-08-29 09:32:48 -0300

    Add Client TDA

---
+++ client.c
@@ -0,0 +1,6 @@
+#include "client.h"
+#include <stdio.h>
+
+void client_init(const char* host, const char* service) {
+  printf("Me conectare al host: %s, en el puerto: %s \n", host, service);
+}
---
+++ client.h
@@ -0,0 +1,6 @@
+#ifndef _CLIENT_H_
+#define _CLIENT_H_
+
+void client_init(const char* host, const char* service);
+
+#endif
