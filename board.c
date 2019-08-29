bf2956cff0680017fb1ddba90c2b52b5864a590e
aleacevedo <ale.acevedo@live.com.ar>
2019-08-29 12:59:03 -0300

    Modify board

--- board.c
+++ board.c
@@ -2,23 +2,51 @@
 
 #include <stdio.h>
 
-int board_init(board_t* self) {
-  FILE *fileBoard = fopen(PATH_BOARD);
+int board_init(board_t *self)
+{
+  FILE *fileBoard = fopen(PATH_BOARD, "r");
   int row = 0;
-  char line[LEN_STRING+1];
-  if (fileBoard == NULL) return -2;
-  while (!feof(fileBoard)) {
-    fgets(line, LEN_STRING+1, fileBoar);
-    if (row > 8) {
+  char line[LEN_STRING + 1];
+  if (fileBoard == NULL)
+    return -2;
+  while (fgets(line, LEN_STRING + 1, fileBoard) != NULL)
+  {
+    printf("LINEA: %s\n", line);
+    if (row > 8)
+    {
       fclose(fileBoard);
-      return -1;  // El archivo tiene mas de 9 filas
+      return -1; // El archivo tiene mas de 9 filas
     }
-    for (int column = 0; column < 9; clumn++) {
-      if (line[column] == 0) continue;
-      board[row][column]->value = line[column];
-      board[row][column]->ini = true;
-      board[row][column]->empty = false;
+    for (int column = 0; column < 9; column++)
+    {
+      if (line[column] == '0')
+      {
+        self[row][column]->value = ' ';
+        continue;
+      }
+      self[row][column]->value = line[column];
+      self[row][column]->ini = 1;
+    }
+    row++;
+    if (fgets(line, LEN_STRING + 1, fileBoard) == NULL)
+    {
+      break;
     }
   }
-  return fclose(fileBoard)
+  return fclose(fileBoard);
+}
+
+int board_set(board_t *self, int row, int column, int value)
+{
+  return 0;
+}
+
+char board_get(board_t *self, int row, int column)
+{
+  return self[row][column]->value;
+}
+
+int board_reset(board_t *self)
+{
+  return 0;
 }
--- board.h
+++ board.h
@@ -4,20 +4,20 @@
 #define PATH_BOARD "./board.txt"
 #define LEN_STRING 9
 
-typedef struct {
-  char value = " ";
-  boolean ini = false;
-  boolean empty = true;
-} cell_t
+typedef struct
+{
+  char value;
+  int ini;
+} cell_t;
 
-typedef cell_t board_t[9][9]
+typedef cell_t board_t[9][9];
 
-int board_init(board_t* self);
+int board_init(board_t *self);
 
-int board_set(board_t* self, int row, int column, int value);
+int board_set(board_t *self, int row, int column, int value);
 
-int board_get(board_t* self, int row, int column)
+char board_get(board_t *self, int row, int column);
 
-int board_reset(board_t* self);
+int board_reset(board_t *self);
 
 #endif
