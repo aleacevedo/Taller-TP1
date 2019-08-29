ee93de79d1a82aac80d3a932929d683f1a0a30da
alejoacevedo <ale.acevedo@live.com.ar>
2019-08-29 09:32:52 -0300

    Add main

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include "client.h"
+#include "server.h"
+
+int main(int argc, const char* argv[]) {
+  if (argc < 2) {
+    printf("Modo no soportado, el primer parámetro debe ser server o client\n​");
+    return -1;
+  }
+  if (strcmp(argv[1], "server") == 0) {
+    if (argc == 3) {
+      printf("Server\n");
+      server_init(argv[2]);
+      return 0;
+    }
+    printf("Uso: ./tp client <host> <puerto>\n");
+    return 1;
+  }
+  if (strcmp(argv[1], "client") == 0) {
+    if (argc == 4) {
+      printf("Cliente\n");
+      client_init(argv[2], argv[3]);
+      return 0;
+    }
+    printf("Uso: ./tp client <host> <puerto>\n");
+    return 1;
+  }
+  printf("Modo no soportado, el primer parámetro debe ser server o client\n​");
+  return -1;
+}
