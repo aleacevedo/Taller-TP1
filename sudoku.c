83bad77ac47e67a7e7b6327996169fe63df2f17b
aleacevedo <ale.acevedo@live.com.ar>
2019-08-29 12:59:17 -0300

    Modify sudoku

--- sudoku.c
+++ sudoku.c
@@ -1,20 +1,49 @@
 #include "sudoku.h"
+#include <stdio.h>
+#include <string.h>
 #include "board.h"
 
-int sudoku_init(sudoku_t self) {
-  return board_init(self->board)
+int sudoku_init(sudoku_t *self)
+{
+  return board_init(&(self->board));
 }
 
-char* sudoku_show(sudoku_t self) {
-  char[BOARD_REPRESENTATION_LEN] boardRepresentation = "\0"
-  for (int row = 0; row < 9; row++) {
-    if (row % 3 == 0) {
-      snprintf(board, BOARD_REPRESENTATION_LEN, "%s%s", board, SECTOR_DIVISOR);
-    } else {
-      snprintf(board, BOARD_REPRESENTATION_LEN, "%s%s", board, ROW_DIVISOR);
+char *sudoku_show(sudoku_t *self, char *boardRepresentation)
+{
+  for (int row = 0; row < 9; row++)
+  {
+    if (row % 3 == 0)
+    {
+      snprintf(boardRepresentation + strlen(boardRepresentation), BOARD_REPRESENTATION_LEN - strlen(boardRepresentation), "%s", SECTOR_DIVISOR);
     }
-    for (int column = 0; column < 9; column++) {
-      snprintf(board, "")
+    else
+    {
+      snprintf(boardRepresentation + strlen(boardRepresentation), BOARD_REPRESENTATION_LEN - strlen(boardRepresentation), "%s", ROW_DIVISOR);
     }
+    for (int column = 0; column < 9; column = column + 3)
+    {
+      char valor0 = board_get(&(self->board), row, column);
+      char valor1 = board_get(&(self->board), row, column + 1);
+      char valor2 = board_get(&(self->board), row, column + 2);
+      snprintf(boardRepresentation + strlen(boardRepresentation), BOARD_REPRESENTATION_LEN - strlen(boardRepresentation), CELL_REPRESENTATION, valor0, valor1, valor2);
+    }
+    snprintf(boardRepresentation + strlen(boardRepresentation), BOARD_REPRESENTATION_LEN - strlen(boardRepresentation), "%s", "U\n");
   }
+  snprintf(boardRepresentation + strlen(boardRepresentation), BOARD_REPRESENTATION_LEN - strlen(boardRepresentation), "%s", SECTOR_DIVISOR);
+  return boardRepresentation;
+}
+
+int sudoku_set(sudoku_t *self, int row, int column, int value)
+{
+  return 0;
+}
+
+int sudoku_validate(sudoku_t *self)
+{
+  return 0;
+}
+
+int sudoku_reset(sudoku_t *self)
+{
+  return 0;
 }
--- sudoku.h
+++ sudoku.h
@@ -6,20 +6,22 @@
 #define SECTOR_DIVISOR "U===========U===========U===========U\n"
 #define ROW_DIVISOR "U---+---+---U---+---+---U---+---+---U\n"
 #define BOARD_REPRESENTATION_LEN 722
+#define CELL_REPRESENTATION "U %c | %c | %c "
 
-typedef struct {
+typedef struct
+{
   board_t board;
-  boolean valid = true;
-} sudoku_t
+  int valid;
+} sudoku_t;
 
-int sudoku_init(sudoku_t* self);
+int sudoku_init(sudoku_t *self);
 
-int sudoku_set(sudoku_t* self, int row, int column, int value);
+int sudoku_set(sudoku_t *self, int row, int column, int value);
 
-char* sudoku_show(sudoku_t* self);
+char *sudoku_show(sudoku_t *self, char *boardRepresentation);
 
-boolean sudoku_validate(sudoku_t* self);
+int sudoku_validate(sudoku_t *self);
 
-int sudoku_reset(sudoku_t* self);
+int sudoku_reset(sudoku_t *self);
 
 #endif
